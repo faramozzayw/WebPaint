361d8a183984d5a8143c3966c76fa58433ee051e
faramo_zayw <faramo.zayw@gmail.com>
2019-04-22 20:26:19 +0300

    Added: basic drawing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './../css/App.css';
 import Panel from './Panel';
+import Canvas from './Canvas';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Panel />
+        <Canvas />
       </div>
     );
   }
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
 
 export default class Canvas extends Component {
+	state = {
+		isDrawing: false,
+		lastX: 0,
+		lastY: 0
+	}
+
+	getCanvas = () => {
+		return this.refs.canvas;
+	}
+
+	getCanvasContext = () => {
+		return this.getCanvas().getContext('2d');
+	}
+
+	componentDidMount() {
+		const canvas = this.getCanvas();
+		const ctx = this.getCanvasContext();
+
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+
+		ctx.strokeStyle = "orange";
+		ctx.lineJoin = "round";
+		ctx.lineCap = "round";
+		ctx.lineWidth = 10;
+	}
+
+	draw = e => {
+		const ctx = this.getCanvasContext();
+
+		if (this.state.isDrawing) {
+			ctx.beginPath();
+			ctx.strokeStyle = 'orange';
+			ctx.moveTo(this.state.lastX, this.state.lastY);
+			ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+			ctx.stroke();
+
+			this.setState({
+				lastX: e.nativeEvent.offsetX,
+				lastY: e.nativeEvent.offsetY
+			})
+		}
+	}
+
 	render() {
 		return (
-			<div>
-				
-			</div>
+				<canvas 
+					ref="canvas" 
+					className="uk-width-1-1"
+					onMouseMove={this.draw.bind(this)}
+					onMouseDown={e => {
+						this.setState({
+							isDrawing: true,
+							lastX: e.nativeEvent.offsetX,
+							lastY: e.nativeEvent.offsetY
+						})}
+					}
+					onMouseUp={e => {
+						this.setState({
+							isDrawing: false
+						})
+					}}
+					onMouseOut={e => {
+						this.setState({
+							isDrawing: false
+						})
+					}}
+					>
+				</canvas>
 		);
 	}
 }
