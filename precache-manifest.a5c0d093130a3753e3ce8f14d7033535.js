5e31f4627f5222d742b190c53ca34dfc6ebf3d2d
faramo_zayw <faramo.zayw@gmail.com>
2019-04-26 17:56:47 +0300

    Updates

---
+++ precache-manifest.a5c0d093130a3753e3ce8f14d7033535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b9290e34e1c833c7f6",
+    "url": "/WebPaint/static/css/main.dae53096.chunk.css"
+  },
+  {
+    "revision": "b8b9290e34e1c833c7f6",
+    "url": "/WebPaint/static/js/main.9827a162.chunk.js"
+  },
+  {
+    "revision": "fb0429021aaccacedb0e",
+    "url": "/WebPaint/static/js/runtime~main.e73fb69d.js"
+  },
+  {
+    "revision": "99551ead966c055de68f",
+    "url": "/WebPaint/static/js/2.42f1ab7c.chunk.js"
+  },
+  {
+    "revision": "18863c6d327044a4ed1b52fbc83bd432",
+    "url": "/WebPaint/static/media/pipette.18863c6d.png"
+  },
+  {
+    "revision": "831227480636fa9a0f18fe51c1aae206",
+    "url": "/WebPaint/static/media/eraser.83122748.png"
+  },
+  {
+    "revision": "f0a685c16141a738c717c220fb0e3208",
+    "url": "/WebPaint/index.html"
+  }
+];
